a464a084c57f80e6e737c9a4bea273d75538d3b6
Aspirata <Const.rv2007@gmail.com>
2023-10-26 19:44:08 +0700

    Optimization

--- Optimize.py
+++ Optimize.py
@@ -1,9 +1,14 @@
-import bpy
+#import bpy
+import os
 
-selected_object = bpy.context.active_object
+# Получите путь к текущей директории вашего скрипта
+script_directory = os.path.dirname(os.path.realpath(__file__))
+print(script_directory)
 
-geonodes_modifier = selected_object.modifiers.new('Camera Culling', type='NODES')
-geonodes_modifier.node_group = bpy.data.node_groups.get("Camera Culling")
-geonodes_modifier["Input_2"] = 0.88
-geonodes_modifier["Input_3"] = bpy.context.scene.camera
-geonodes_modifier.show_render = False
+#selected_object = bpy.context.active_object
+
+#geonodes_modifier = selected_object.modifiers.new('Camera Culling', type='NODES')
+#geonodes_modifier.node_group = bpy.data.node_groups.get("Camera Culling")
+#geonodes_modifier["Input_2"] = 0.88
+#geonodes_modifier["Input_3"] = bpy.context.scene.camera
+#geonodes_modifier.show_render = False
