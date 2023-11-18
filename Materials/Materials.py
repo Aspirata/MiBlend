623d9c979c4bf14f83fbfc503d3b13ca9dc87d16
Aspirata <Const.rv2007@gmail.com>
2023-11-19 00:57:34 +0700

    Upgrade materials for every selected object

--- Materials/Materials.py
+++ Materials/Materials.py
@@ -16,6 +16,7 @@ def append_materials(upgraded_material_name, selected_object, i):
         selected_object.data.materials[i] = bpy.data.materials[upgraded_material_name]
 
 def upgrade_materials():
+<<<<<<< Updated upstream
     selected_object = bpy.context.active_object
     for i, material in enumerate(selected_object.data.materials):
         if "water_still" in material.name.lower():
@@ -23,6 +24,50 @@ def upgrade_materials():
             
         if "water_flow" in material.name.lower():
             append_materials("Upgraded water flow", selected_object, i)
+=======
+    selected_objects = bpy.context.selected_objects
+    for selected_object in selected_objects:
+        for i, material in enumerate(selected_object.data.materials):
+
+            if "water_still" in material.name.lower():
+                append_materials("Upgraded Water Still", selected_object, i)
+                
+            if "water_flow" in material.name.lower():
+                append_materials("Upgraded Water Flow", selected_object, i)
+
+            if "sculk" in material.name.lower():
+                append_materials("Upgraded Sculk", selected_object, i)
+            
+            if "glow_lichen" in material.name.lower():
+                append_materials("Upgraded Glow Lichen", selected_object, i)
+
+            if "stone" in material.name.lower():
+                append_materials("Upgraded Stone", selected_object, i)
+
+            if "sand" in material.name.lower():
+                append_materials("Upgraded Sand", selected_object, i)
+
+            if "bricks" in material.name.lower():
+                append_materials("Upgraded Bricks", selected_object, i)
+
+            if "iron_block" in material.name.lower():
+                append_materials("Upgraded Iron Block", selected_object, i)
+
+            if "gold_block" in material.name.lower():
+                append_materials("Upgraded Gold Block", selected_object, i)
+
+            if "diamond_ore" in material.name.lower():
+                append_materials("Upgraded Diamond Ore", selected_object, i)
+
+            if "lantern" in material.name.lower():
+                append_materials("Upgraded Lantern", selected_object, i)
+
+            if "soul_lantern" in material.name.lower():
+                append_materials("Upgraded Soul Lantern", selected_object, i)
+
+            if "obsidian" in material.name.lower():
+                append_materials("Upgraded Obsidian", selected_object, i)
+>>>>>>> Stashed changes
 
 
 #Fix materials
