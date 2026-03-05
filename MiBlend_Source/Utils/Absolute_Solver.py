c3b7d64ae541a6212965f03ad004620d8b2bdf64
Aspirata <Const.rv2007@gmail.com>
2026-03-05 19:17:55 +0700

    Deleted Translator.py

--- MiBlend_Source/Utils/Absolute_Solver.py
+++ MiBlend_Source/Utils/Absolute_Solver.py
@@ -1,6 +1,15 @@
 import bpy, os, time, json, traceback
 from ..Data import utils_directory
-from ..Utils.Translator import translate
+
+
+def translate(untranslated_string: str) -> str:
+    current_language = bpy.app.translations.locale
+
+    with open(os.path.join(utils_directory, "languages", f"{current_language}.json"), "r") as file:
+        translations_dict: dict[str, str] = json.load(file)
+
+    return translations_dict.get(untranslated_string, untranslated_string)
+
 
 def Call_AS(code: str, tech_things: str = "", data: str = ""):
     Preferences = bpy.context.preferences.addons[str(__package__).split(".")[0]].preferences
@@ -64,6 +73,7 @@ def Call_AS(code: str, tech_things: str = "", data: str = ""):
         Call_AS.call_queue.clear()
         Call_AS.last_call_time = current_time
         Call_AS.is_processing = False
+
 
 class AbsoluteSolverPanel(bpy.types.Operator):
     bl_label = "Absolute Solver"
@@ -139,6 +149,7 @@ class AbsoluteSolverPanel(bpy.types.Operator):
 
     def execute(self, context):
         return {'FINISHED'}
+
 
 class AbsoluteSolverIgnore(bpy.types.Operator):
     bl_idname = "special.as_ignore"
