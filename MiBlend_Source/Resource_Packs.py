Aspirata/MiBlend

commit df00231b1426de136f688a7e6c377028d261f2ef

Some Fixes

MiBlend_Source/Resource_Packs.py
@@ -62,7 +62,7 @@ def find_mc() -> tuple[str, str]:
         folders = Preferences.mc_instances_path if Preferences.mc_instances_path else os.path.join(os_env, path)
         if not os.path.isdir(folders):
             continue
-
+        
         for folder in os.listdir(folders):
             instance_dir = os.path.join(folders, folder)
             if not os.path.isdir(instance_dir):
@@ -83,7 +83,7 @@ def find_mc() -> tuple[str, str]:
     if versions:
         latest_version = max(versions, key=lambda x: LooseVersion(x))
         latest_file, latest_path = versions[latest_version]
-        return latest_version, os.path.join(instance_dir, jar_file)
+        return latest_version, latest_path
     
     return "", ""
 

MiBlend_Source/MIB_API.py
@@ -75,6 +75,9 @@ def get_selected_asset() -> dict:
 def mc_version_formatter(version_name: str) -> str:
     try:
         version_parts = re.split(r'[ -]', version_name)
+        if "snapshot" in version_parts:
+            return ""
+
         for part in version_parts:
             if not any(char.isalpha() for char in part) and re.match(r'^\d{1,2}\.\d{1,2}(?:\.\d{1,2})?$', part):
                 return part
@@ -131,13 +134,22 @@ def get_pack_info_properties(pack_name: str = "") -> dict:
     if not os.path.exists(resource_packs_directory):
         return {}
     
-    with open(os.path.join(resource_packs_directory, "packs_info.json"), "r") as file:
-        data = json.load(file)
-        
-        if not pack_name:
-            return data.keys()
-        
-        return data.get(pack_name, {})
+    packs_info_path = os.path.join(resource_packs_directory, "packs_info.json")
+    try:
+        with open(packs_info_path, "r") as file:
+            data = json.load(file)
+            
+            if not pack_name:
+                return data.keys()
+            
+            return data.get(pack_name, {})
+    except FileNotFoundError:
+        with open(packs_info_path, "w") as file:
+            json.dump({}, file)
+        return {}
+    except:
+        Call_AS("n00", traceback.format_exc())
+        return {}
 
 def is_code_ignored(code: str) -> bool:
     return code in bpy.context.scene.miblend_properties.absolute_solver_properties.ignored_codes.split()

commit 82ab5bfc5dca885120b6803e9f4c74f4375874a3

Fixed inability to apply MC resource pack
This bug was introduced in https://github.com/Aspirata/MiBlend/commit/df00231b1426de136f688a7e6c377028d261f2ef#diff-6c8995d40261c995773bfb14345d48a8f4081a37635514ce4caf1b60ee977889

.gitignore
@@ -1,4 +1,5 @@
 build
-MiBlend.zip
+*.zip
+2*
 1.*
 .DS_Store

MiBlend_Source/Resource_Packs.py
@@ -78,14 +78,14 @@ def find_mc() -> tuple[str, str]:
             instance_path = os.path.join(instance_dir, jar_file)
             version = mc_version_formatter(folder)
             if version and os.path.isfile(instance_path):
-                versions[version] = (jar_file, instance_dir)
+                versions[version] = (jar_file, instance_path)
                 dprint(f"{instance_path} valid", is_deep=True, zone="rp")
             else:
                 dprint(f"{instance_path} invalid", is_deep=True, zone="rp")
         
     if versions:
         latest_version = max(versions, key=lambda x: LooseVersion(x))
-        latest_file, latest_path = versions[latest_version]
+        _latest_file, latest_path = versions[latest_version]
         return latest_version, latest_path
     
     return "", ""