e0d71cc9fd170239c57a8852d37de7a9082ee811
Aspirata <Const.rv2007@gmail.com>
2026-08-07 15:12:37 +0700

    Fixed Cracks Overlay and Enchantment Assets

--- MiBlend_Source/Assets/Nodes/Shader Nodes/Cracks/Cracks.py
+++ MiBlend_Source/Assets/Nodes/Shader Nodes/Cracks/Cracks.py
@@ -2,19 +2,18 @@ active_obj = bpy.context.active_object
 if active_obj and active_obj.active_material:
     current_material = active_obj.active_material
     if current_material.use_nodes:
-        PBSDF = None
-        co_node = None
+        PBSDF = next((node for node in current_material.node_tree.nodes if node.type == "BSDF_PRINCIPLED"), None)
+        if PBSDF:
+            node_group = create_node_group(current_material, "Cracks Overlay", (PBSDF.location.x - 170, PBSDF.location.y), get_selected_asset().get("File_path"), True)
+            if node_group:
+                base_color_connection = get_connected_socket_to("Base Color", PBSDF)
+                if not base_color_connection or base_color_connection.node != node_group:
+                    if base_color_connection:
+                        current_material.node_tree.links.new(base_color_connection, node_group.inputs["Color"])
+                    current_material.node_tree.links.new(node_group.outputs["Color"], PBSDF.inputs["Base Color"])
 
-        for node in current_material.node_tree.nodes:
-            if node.type == "BSDF_PRINCIPLED":
-                PBSDF = node
-
-        node_group = create_node_group(current_material, "Cracks Overlay", (PBSDF.location.x - 170, PBSDF.location.y), get_selected_asset().get("File_path"), True)
-
-        if GetConnectedSocketTo("Base Color", PBSDF).node != node_group:
-            current_material.node_tree.links.new(GetConnectedSocketTo("Base Color", PBSDF), co_node.inputs["Color"])
-            current_material.node_tree.links.new(co_node.outputs["Color"], PBSDF.inputs["Base Color"])
-
-        if GetConnectedSocketTo("Alpha", PBSDF).node != node_group:
-            current_material.node_tree.links.new(GetConnectedSocketTo("Alpha", PBSDF), co_node.inputs["Alpha"])
-            current_material.node_tree.links.new(co_node.outputs["Alpha"], PBSDF.inputs["Alpha"])
+                alpha_connection = get_connected_socket_to("Alpha", PBSDF)
+                if not alpha_connection or alpha_connection.node != node_group:
+                    if alpha_connection:
+                        current_material.node_tree.links.new(alpha_connection, node_group.inputs["Alpha"])
+                    current_material.node_tree.links.new(node_group.outputs["Alpha"], PBSDF.inputs["Alpha"])
--- MiBlend_Source/Assets/Nodes/Shader Nodes/Enchant/Enchant.py
+++ MiBlend_Source/Assets/Nodes/Shader Nodes/Enchant/Enchant.py
@@ -3,31 +3,22 @@ import bpy
 active_obj = bpy.context.active_object
 if active_obj and active_obj.active_material:
     current_material = active_obj.active_material
-    if not current_material.use_nodes:
-        exit()
-        
-    PBSDF = None
+    if current_material.use_nodes:
+        PBSDF = next((node for node in current_material.node_tree.nodes if node.type == "BSDF_PRINCIPLED"), None)
+        if PBSDF:
+            node_group = create_node_group(current_material, "Enchantment", (PBSDF.location.x - 200, PBSDF.location.y - 280), get_selected_asset().get("File_path"), True)
+            if node_group:
+                if socket := get_connected_socket_to("Emission Color", PBSDF):
+                    if socket.node != node_group:
+                        current_material.node_tree.links.new(socket, node_group.inputs["Multiply Color"])
 
-    for node in current_material.node_tree.nodes:
-        if node.type == "BSDF_PRINCIPLED":
-            PBSDF = node
+                if socket := get_connected_socket_to("Emission Strength", PBSDF):
+                    if socket.node != node_group:
+                        current_material.node_tree.links.new(socket, node_group.inputs["Multiply"])
 
-    if not PBSDF:
-        exit()
-        
-    node_group = create_node_group(current_material, "Enchantment", (PBSDF.location.x - 200, PBSDF.location.y - 280), get_selected_asset().get("File_path"), True)
+                current_material.node_tree.links.new(node_group.outputs[0], PBSDF.inputs["Emission Color"])
+                current_material.node_tree.links.new(node_group.outputs[1], PBSDF.inputs["Emission Strength"])
 
-    if socket := GetConnectedSocketTo(PBSDF_compability("Emission Color"), PBSDF):
-        if socket.node != node_group:
-            current_material.node_tree.links.new(socket, node_group.inputs["Multiply Color"])
-    
-    if socket := GetConnectedSocketTo("Emission Strength", PBSDF):
-        if socket.node != node_group:
-            current_material.node_tree.links.new(socket, node_group.inputs["Multiply"])
-
-    current_material.node_tree.links.new(node_group.outputs[0], PBSDF.inputs[PBSDF_compability("Emission Color")])
-    current_material.node_tree.links.new(node_group.outputs[1], PBSDF.inputs["Emission Strength"])
-    
-    node_group.inputs["Divider"].default_value = properties.get("Divider")
-    node_group.inputs["Camera Strenght"].default_value = properties.get("Camera Strength")
-    node_group.inputs["Non-Camera Strenght"].default_value = properties.get("Non-Camera Strength")
+                node_group.inputs["Divider"].default_value = properties.get("Divider")
+                node_group.inputs["Camera Strenght"].default_value = properties.get("Camera Strength")
+                node_group.inputs["Non-Camera Strenght"].default_value = properties.get("Non-Camera Strength")
